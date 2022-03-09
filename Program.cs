742200220372e23ae0cdb62370dac2b9e7110da6
Fil31 <lolid76@gmail.com>
2022-03-09 11:58:03 +0300

    add my first c# project

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your name?");
+            string userFirstName = Console.ReadLine();
+            Console.WriteLine("What is your last name?");
+            string userLastName = Console.ReadLine();
+            Console.WriteLine("Привет," + userFirstName + " " + userLastName + "!");
+        }
+    }
+}
